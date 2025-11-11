7463f9a99661c5e700e94c348d446af7ab243659
Leonardo <leonardorscarpitta@gmail.com>
2025-11-10 23:07:13 -0300

    feat(fix): fix enum errors

--- src/main/java/io/github/humaniza/vitaflow/dto/RegistroDiarioResponseDTO.java
+++ src/main/java/io/github/humaniza/vitaflow/dto/RegistroDiarioResponseDTO.java
@@ -1,16 +1,19 @@
 package io.github.humaniza.vitaflow.dto;
 
+import io.github.humaniza.vitaflow.model.EstadoMental;
 import io.github.humaniza.vitaflow.model.RegistroDiario;
 
 public record RegistroDiarioResponseDTO(Integer idPaciente,
-                                        String estadoMental,
+                                        EstadoMental estadoMental,
                                         Integer qualidadeSono,
                                         Integer qualidadeHumor) {
 
     public RegistroDiarioResponseDTO(RegistroDiario registroDiario) {
-        this.idPaciente = registroDiario.getIdPaciente().getId();
-        this.estadoMental = registroDiario.getEstadoMental();
-        this.qualidadeSono = registroDiario.getQualidadeSono();
-        this.qualidadeHumor = registroDiario.getQualidadeHumor();
+        this(
+                registroDiario.getIdPaciente().getId(),
+                registroDiario.getEstadoMental(),
+                registroDiario.getQualidadeSono(),
+                registroDiario.getQualidadeHumor()
+        );
     }
 }
