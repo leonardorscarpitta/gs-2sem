05a7df609e604db8dafc484de4d7d4688bf55453
Leonardo <leonardorscarpitta@gmail.com>
2025-11-13 15:19:13 -0300

    feat: improve exceptions handler

--- src/main/java/io/github/humaniza/vitaflow/exception/GlobalExceptionHandler.java
+++ src/main/java/io/github/humaniza/vitaflow/exception/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package io.github.humaniza.vitaflow.exception;
 
+import jakarta.validation.ConstraintViolationException;
+import org.hibernate.validator.internal.engine.ConstraintViolationImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -17,7 +21,7 @@ public class GlobalExceptionHandler {
         errorResponse.put("timestamp", LocalDateTime.now());
         errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
         errorResponse.put("error", "Dados inválidos");
-        errorResponse.put("message", ex.getMessage());
+        errorResponse.put("message", ex.getCause());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
@@ -31,13 +35,33 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
+    @ExceptionHandler(NaoAutorizadoException.class)
+    public ResponseEntity<HashMap<String, Object>> handleNaoAutorizado(NaoAutorizadoException ex) {
+        HashMap<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("timestamp", LocalDateTime.now());
+        errorResponse.put("status", HttpStatus.UNAUTHORIZED.value());
+        errorResponse.put("error", "Você não tem permissão para acessar esse conteudo!");
+        errorResponse.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<HashMap<String, Object>> handleNaoEncontrado(NoResourceFoundException ex) {
+        HashMap<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("timestamp", LocalDateTime.now());
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+        errorResponse.put("error", "Esse recurso não existe!");
+        errorResponse.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<HashMap<String, Object>> handleGenericException(Exception ex) {
         HashMap<String, Object> errorResponse = new HashMap<>();
         errorResponse.put("timestamp", LocalDateTime.now());
         errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
         errorResponse.put("error", "Erro interno");
-        errorResponse.put("message", "Ocorreu um erro inesperado");
+        errorResponse.put("message", ex.getClass().getName());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 
---
+++ src/main/java/io/github/humaniza/vitaflow/exception/NaoAutorizadoException.java
@@ -0,0 +1,11 @@
+package io.github.humaniza.vitaflow.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class NaoAutorizadoException extends RuntimeException {
+    public NaoAutorizadoException(String message) {
+        super(message);
+    }
+}
